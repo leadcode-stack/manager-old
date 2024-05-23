be6e5a6b14cf28603fbb2639fc4161fb7f027821
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-05-23 13:27:00 +0200

    feat: enhance architecture

--- lib/src/features/authentication/presentation/login_screen.dart
+++ lib/src/features/authentication/presentation/login_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:manager/src/commons/widgets/form/input_control.dart';
-import 'package:manager/src/features/authentication/data/controllers/auth_controller.dart';
-import 'package:manager/src/features/authentication/data/validators/auth_validator.dart';
+import 'package:manager/src/features/authentication/domains/controllers/auth_controller.dart';
+import 'package:manager/src/features/authentication/domains/validators/auth_validator.dart';
 
 final class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
