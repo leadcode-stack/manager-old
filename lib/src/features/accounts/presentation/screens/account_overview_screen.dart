57329de23cf9c5ca04fab6a6c2bd0315a677230c
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-05-26 11:29:36 +0200

    feat: enhance account navigation

--- lib/src/features/accounts/presentation/screens/account_overview_screen.dart
+++ lib/src/features/accounts/presentation/screens/account_overview_screen.dart
@@ -14,66 +14,53 @@ class AccountOverviewScreen extends StatefulWidget {
 
 class _AccountOverviewScreenState extends State<AccountOverviewScreen>
     with TickerProviderStateMixin {
-  late final TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final location = GoRouterState.of(context).path;
-    _tabController.index = switch (location) {
-      final location when location!.startsWith('/accounts/users') => 0,
-      final location when location!.startsWith('/accounts/roles') => 1,
-      _ => 0,
-    };
+    final location = GoRouterState.of(context).matchedLocation;
+    int getInitialIndex() => switch (location) {
+          final path when path.startsWith('/accounts/users') => 0,
+          final path when path.startsWith('/accounts/roles') => 1,
+          _ => 0,
+        };
 
     return Scaffold(
       appBar: const OverviewAppBar(
         title: 'Accounts',
         description: Text('Discover and manage users, roles and sessions.'),
       ),
-      body: Container(
-          color: Colors.grey.shade200,
-          child: Column(
-            children: [
-              Container(
-                color: Colors.white,
-                child: TabBar(
-                    isScrollable: true,
-                    controller: _tabController,
-                    labelColor: Colors.blue.shade400,
-                    indicatorColor: Colors.blue.shade400,
-                    dividerColor: Colors.grey.shade200,
-                    onTap: (index) {
-                      return switch (index) {
-                        0 => context.go('/accounts/users'),
-                        1 => context.go('/accounts/roles'),
-                        _ => null,
-                      };
-                    },
-                    tabs: const [
-                      Tab(text: 'Members'),
-                      Tab(text: 'Roles'),
-                    ]),
-              ),
-              Expanded(
-                child: TabBarView(controller: _tabController, children: const [
-                  UsersListScreen(),
-                  RolesListScreen()
-                ]),
-              ),
-            ],
-          )),
+      body: DefaultTabController(
+        initialIndex: getInitialIndex(),
+        length: 2,
+        child: Container(
+            color: Colors.grey.shade200,
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: TabBar(
+                      isScrollable: true,
+                      labelColor: Colors.blue.shade400,
+                      indicatorColor: Colors.blue.shade400,
+                      dividerColor: Colors.grey.shade200,
+                      onTap: (index) {
+                        return switch (index) {
+                          0 => context.go('/accounts/users'),
+                          1 => context.go('/accounts/roles'),
+                          _ => null,
+                        };
+                      },
+                      tabs: const [
+                        Tab(text: 'Members'),
+                        Tab(text: 'Roles'),
+                      ]),
+                ),
+                const Expanded(
+                  child: TabBarView(
+                      children: [UsersListScreen(), RolesListScreen()]),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
